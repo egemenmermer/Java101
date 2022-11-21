0fd6097a1a5d7f8c3024f8cde3af8134e6df18b9
Egemen Mermer <egemenmermer@gmail.com>
2022-11-21 23:51:05 +0300

    odev

---
+++ src/CinZodyagi.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class CinZodyagi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+
+        System.out.println("Doğum Yılınızı Giriniz : ");
+        year = sc.nextInt();
+        int remain = year % 12;
+
+        switch (remain){
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+
+        }
+    }
+}

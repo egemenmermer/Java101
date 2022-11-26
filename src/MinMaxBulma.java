c8be5c5ed41a400f4f49111cbdda5d25bfe10a4c
Egemen Mermer <egemenmermer@gmail.com>
2022-11-26 21:23:45 +0300

    odev

---
+++ src/MinMaxBulma.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class MinMaxBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counter = 0, i = 1, temp,min = 0, max = 0;
+
+        System.out.println("Kac sayi gireceksiniz: ");
+        counter = input.nextInt();
+
+        while (i <= counter){
+            System.out.println(i + ". Sayıyı giriniz: ");
+            temp = input.nextInt();
+
+            if (temp > max){
+                max = temp;
+            }else if (temp < max && max > min){
+                min = temp;
+            }
+            i++;
+        }
+        System.out.println("En büyük sayı: " + max);
+        System.out.println("En kucuk sayı: " + min);
+    }
+}

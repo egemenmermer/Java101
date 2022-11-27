94e5fc228d9c5025d8ef7ad814a76c4936790a8c
Egemen Mermer <egemenmermer@gmail.com>
2022-11-27 19:42:22 +0300

    odev

---
+++ src/RecursiveUs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RecursiveUs {
+    static int power(int a, int b){
+        if(a == 0 || b == 1)
+            return 1;
+        return a * power(a,b-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Taban değeri giriniz : ");
+        int n1 = sc.nextInt();
+        System.out.println("Üs değeri giriniz : ");
+        int n2 = sc.nextInt();
+
+        System.out.println("Sonuc: " + power(n1,n2));
+    }
+}
